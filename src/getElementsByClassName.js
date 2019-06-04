d0aafd1b1524223c21f9d6a2e27337e64edce3af
Collin Fairlie <collin.fairlie@gmail.com>
2019-06-04 14:53:59 -0500

    Finished working throughElements, adding all of the elements to my output array.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,34 +6,15 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-    //console.log (document.getElementsByClassName(className))  ;
-    // console.log('document.body.childNodes', document.body.childNodes)
     let elements = [];
     var findElements = function (element) {
         if (element.classList && element.classList.contains(className)) {
             elements.push(element)
         } 
-        // if (element.childNodes && element.children.contains(className)) {
-        //     elements.push(element)
-        // }
-        //console.log(element.children)
-        console.log(element.children)
-        for (let i = 0; i < element.children.length; i++) {
-            // if (element.children[i] == (className)) {
-            //     elements.push(element.children[i])
-            // }
-            console.log(elements.children[i])
-            findElements(elements.children[i])
+            for (var i = 0; i < element.children.length; i++) {
+            findElements(element.children[i])
         }
-        // return getElementsByClassName (className);
     }
-    // return document.body.classList.contains(className);
     findElements(document.body);
-    //console.log(elements)
     return elements;
-    //console.log(document.body.classList.contains(className))
-    // console.log('document.body', document.body)
-    // console.log('document.body.classList', document.body.classList)
-    
-    // return document.body.childNodes.classList === className ? true : false;
 };
