e44b88e2ff6cdce19aa61c4d8c81c5329d288c49
Collin Fairlie <collin.fairlie@gmail.com>
2019-06-04 17:03:42 -0500

    Working on stringify, getting through objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,59 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  return JSON.stringify(obj)
+  // console.log(JSON.stringify(obj))
+  if (typeof obj === 'string') {
+    return `"${obj}"`
+  }
+  if (typeof obj !== 'object' || obj === null) {
+    return `${obj}`
+  }
+  if (Array.isArray(obj) && obj.length <= 0) {
+    return `[${obj}]`
+  }
+  if (Array.isArray(obj)) {
+    let output = [];
+    for (let i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === 'number') {
+        output.push (obj[i])
+      } else {
+        output.push(stringifyJSON(obj[i]))
+      }
+    }
+    return `[${output}]`
+  }
+  if (typeof obj === 'object' && !Array.isArray(obj)) {
+    // let output = '';
+    if (Object.keys(obj).length <= 0) {
+      return `{}`
+    }
+    // if (Object.keys(obj).length > 0) {
+    //   object(obj)
+    // }
+    // function object (obj) {
+      let output = '{'
+      for (let key in obj) {
+        if (Object.keys(obj).length === 1) {
+          output = output.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + '}')
+          return output;
+        } else {
+          Object.keys(obj).forEach(function(key) { 
+            output = output.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',')
+          })
+        }
+        // output[key] = stringifyJSON(key)
+        // output[key] = obj[key]
+        // console.log(output)
+        // console.log(Object.values(obj))
+        // console.log(output[stringifyJSON(key)] = stringifyJSON(key))
+      }
+      output = output.slice(0, output.length-1)
+      console.log(output + '}')
+      return output + '}';
+    // }
+    // return `{${output}}`
+  }
+
+  // return JSON.stringify(obj)
+
 };
